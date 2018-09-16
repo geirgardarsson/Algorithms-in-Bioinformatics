79a5aaa47c7ed3f212d0cf16644668ee0463432c
geirgardarsson <geg42@hi.is>
2018-09-16 22:34:50 +0000

    Add greedymotifsearch

--- utils.py
+++ utils.py
@@ -1,8 +1,17 @@
 import itertools as it
+import numpy as np
 from collections import Counter
 
 comb = it.combinations_with_replacement 
 
+
+#dna indexes
+di = {
+        "A": 0,
+        "C": 1,
+        "G": 2,
+        "T": 3
+    }
 
 def hammlen(string1, string2, m=0, fast=False):
     h = 0
@@ -74,4 +83,75 @@ def conscore(arr):
         score += m[1]
         
     return (cons, score)
+
+
+def calcscore(arr):
+    arr = np.asarray([list(x) for x in arr])
+    score = arr.shape[0] * arr.shape[1]
+
+    for i in range(arr.shape[1]):
+        col = ''.join(arr[:,i])
+        m = Counter(col).most_common(1)[0]
+        score -= m[1]
+
+    return score
+
+
+def calcprob(kmer, matrix):
+    prob = 1
+
+    for i in range(len(kmer)):
+        prob *= matrix[di[kmer[i]]][i]
+
+    return prob
+
+
+def profilemostprobstring(dna, k, pr):
+    probs = []
+
+    for i in range(len(dna)-k+1):
+        x = dna[i:i+k]
+        probs.append((calcprob(x,pr),x))
+
+    return max(probs, key=lambda a:a[0])[1]
+    
+
+
+
+def makeprofile(motifs):
+    motifs = np.asarray([list(x) for x in motifs])
+    j = motifs.shape[0]
+    motifs = np.transpose(motifs)
+
+    counts = [Counter(x) for x in motifs]
+
+    profiles = [
+        [c['A'],c['C'],c['G'],c['T']]
+        for c in counts
+    ]
+
+    divide = lambda a: a/j
+    vdivide = np.vectorize(divide)
+
+    profiles = [
+        vdivide(x)
+        for x in profiles
+    ]
+
+    profiles = np.transpose(np.asarray(profiles))
+
+    return profiles
+
+
+
+
+
+
+
+
+
+
+
+
+
 
